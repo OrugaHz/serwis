6440174ab1528474bbaaef44f90d953342b2365e
Oruga <kanojodo@gmail.com>
2023-04-17 19:29:00 +0200

    admin login, admin news

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,62 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
+
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('login');
+        return
+    };
+    next();
+});
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('admin', {
-        title: 'Admin'
+    News.find()
+        .then((data) => {
+            res.render('admin/index', {
+                title: 'Admin',
+                data
+            });
+            console.log(data);
+        })
+});
+
+router.get('/news/add', (req, res) => {
+    res.render('admin/news-form', {
+        title: 'Dodaj news',
+        body: {},
+        errors: {}
     });
 });
 
+router.post('/news/add', (req, res) => {
+    const body = req.body;
+
+    const newsData = new News(body);
+    const errors = newsData.validateSync();
+
+
+    newsData.save()
+        .then(() => {
+            res.redirect('/admin')
+        })
+        .catch(function (err) {
+            res.render('admin/news-form', {
+                title: 'Dodaj news',
+                errors,
+                body
+            });
+        });
+});
+
+router.get('/news/remove/:id', (req, res) => {
+    News.pre(findOneAndDelete(req.params.id), function () {
+        res.redirect('/admin')
+    })
+    // .then(() => {
+    //     res.redirect('/admin')
+    // })
+});
 
 module.exports = router;
