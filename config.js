a58cc36c50d9f467aae35e994a5cbf5253c44602
Oruga <kanojodo@gmail.com>
2023-04-18 11:22:08 +0200

    news filter front

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:wavoO0RtPf5axXch@cluster0.hgbou32.mongodb.net/?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000,
 }
