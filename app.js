b3de75ebbce4c8911719c881ef0cacb0205c7aaa
Oruga <kanojodo@gmail.com>
2023-04-23 08:06:19 +0200

    api news

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+var apiRouter = require('./routes/api');
 
 
 var app = express();
@@ -57,6 +58,7 @@ app.use('/', indexRouter);
 app.use('/quiz', quizRouter);
 app.use('/news', newsRouter);
 app.use('/admin', adminRouter);
+app.use('/api', apiRouter);
 
 
 // catch 404 and forward to error handler
